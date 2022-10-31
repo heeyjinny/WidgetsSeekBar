5cd1e4c56f0ffd3f3a2b6b54ecdae49e0e25cae0
heeyjinny <heeyjinny@gmail.com>
2022-11-01 06:11:22 +0900

    시크바가 변경 되었을 때 text값 변경

--- app/src/main/java/com/heeyjinny/widgetsseekbar/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/widgetsseekbar/MainActivity.kt
@@ -2,10 +2,30 @@ package com.heeyjinny.widgetsseekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.heeyjinny.widgetsseekbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.textView.text = "$p1"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+
     }
 }
