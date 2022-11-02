83d23d0b20c7f778be6889b1ff1969bdd23e0c7b
heeyjinny <heeyjinny@gmail.com>
2022-11-03 02:40:52 +0900

    시크바 만들기, 값에따라 텍스트값 변경

--- app/src/main/java/com/heeyjinny/widgetsseekbar/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/widgetsseekbar/MainActivity.kt
@@ -7,14 +7,22 @@ import com.heeyjinny.widgetsseekbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //뷰바인딩
     val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //뷰바인딩
         setContentView(binding.root)
 
+        //시크바가 사용할 수 있는 리스너 한 가지 : 인터페이스 3개 메서드 생성
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+
+            //첫 번째 파라미터 : 리스너가 동작하고 있는 시크바 위젯
+            //두 번째 파라미터 : 현재 시크바의 값
+            //세 번째 파라미터 : 사용자 터치 여부(사용자 터치에 의해 동작 인지를 알기위함)
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                //텍스트뷰에 현재 시크바의 값 입력 : 텍스트뷰는 문자열만 입력이 가능해 $사용하여 변수 입력
                 binding.textView.text = "$p1"
             }
 
@@ -26,6 +34,6 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+    }//onCreate
 
-    }
-}
+}//MainActivity
